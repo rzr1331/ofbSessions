e8fca0d7ccc7884f9b4ad484f179c8617b11fc9d
Avichal Sahai <avichalsahai@Avichals-MacBook-Pro.local>
2018-10-17 17:14:23 +0530

    Assignment Completed

--- src/Assignment4/FileIOEngine.java
+++ src/Assignment4/FileIOEngine.java
@@ -2,6 +2,7 @@ package Assignment4;
 
 import com.sun.xml.internal.xsom.impl.scd.Iterators;
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -11,37 +12,36 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.logging.Logger;
 
 public class FileIOEngine {
+    private static Logger logger = Logger.getLogger(FileIOEngine.class.getName());
 
-    public static void main (String[] args) throws IOException {
-        System.out.println("free memory : " + Runtime.getRuntime().freeMemory());
+    public static void main(String[] args) throws IOException {
+        logger.info("free memory : " + Runtime.getRuntime().freeMemory());
         File sourceFile = new File("/Users/avichalsahai/temp_1GB_file");
+        File destinationFile = new File("/Users/avichalsahai/copied_1GB_file");
 
         System.out.println("file length : " + sourceFile.length());
 
         InputStream inputStream = new BufferedInputStream(new FileInputStream(sourceFile));
-        byte[] chunk = new byte[1024];
-        int offset = 0;
-        int length = 1024;
-        int count =0 ;
-        while (offset <= sourceFile.length()) {
-            int readCount = inputStream.read(chunk, offset, length);
-            System.out.println("readCount : " + readCount);
-            System.out.println("free memory : " + Runtime.getRuntime().freeMemory());
+        OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(destinationFile));
 
-            FileOutputStream fileOutputStream = new FileOutputStream("/Users/avichalsahai/copied_1GB_file");
-            fileOutputStream.write(chunk);
-            offset+=readCount;
+        byte[] chunk = new byte[10000];
+        long readerExitCode = 1;
+        int counter = 0;
+        while (readerExitCode >= 0) {
+            readerExitCode = inputStream.read(chunk);
+            outputStream.write(chunk);
+            counter ++;
         }
+        logger.info("Copied File : " + destinationFile.getName());
+        logger.info("File Size : " + destinationFile.length());
+        logger.info("Number Of Chunks : " + counter);
 
-        System.out.println("Copied file length : " + new File("/Users/avichalsahai/copied_1GB_file").length());
-
-
-
-
-
+        outputStream.flush();
+        outputStream.close();
+        inputStream.close();
 
     }
-
 }
